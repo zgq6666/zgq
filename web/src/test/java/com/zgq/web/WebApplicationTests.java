56f2a6aa5ae9da449b606e7e9bd24df700267697
zhang <2322202359@qq.com>
2020-05-12 15:03:19 +0800

    先搭框架

---
+++ web/src/test/java/com/zgq/web/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zgq.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
